00b5b3323473f38bfb7662f4bfc03d9596f62ff3
Farah Mahmoud <faraheltahawy27@gmail.com>
2026-02-10 01:38:59 +0200

    setup

---
+++ awwwards-winning-website/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <h1 className='text-2xl text-orange-500 font-bold'>hello</h1>
+    </main>
+  )
+}
+
+export default App
